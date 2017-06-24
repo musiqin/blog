8ce5c0365ad7895bff201bbcae1cd7b691cdc373
mobihack <aaduthoma19118@gmail.com>
2017-06-24 17:39:01 +0530

    Added Full Table of Contents
    With sticky sidenav

--- static/js/bulma.js
+++ static/js/bulma.js
@@ -9,7 +9,6 @@
          menu.classList.toggle('is-active');
      });
  })();
- 
  //Generate hash
  String.prototype.hashCode = function() {
      var hs = 0,
@@ -22,7 +21,6 @@
      }
      return (hs >>> 0);
  };
-
  //Generate Identicons
  var xv = document.getElementsByClassName('identicons');
  if (xv.length > 0) {
@@ -32,3 +30,58 @@
      }
      jdenticon();
  }
+
+ function outerHeight(elem){
+    var curStyle = elem.currentStyle || window.getComputedStyle(elem);
+    outerHeight = elem.offsetHeight;
+    outerHeight += parseInt(curStyle.marginTop);
+    outerHeight += parseInt(curStyle.marginBottom);
+
+    return outerHeight //If you'd like to return the outerheight
+}
+
+ (function() {
+     'use strict';
+     var left = document.getElementById("fixed-toc"),
+         content = document.getElementById("post-content"),
+         start = left.offsetTop + 380,
+         stop = content.offsetTop + outerHeight(content),
+         docBody = document.documentElement || document.body.parentNode || document.body,
+         hasOffset = window.pageYOffset !== undefined,
+         scrollTop;
+     var section = document.querySelectorAll("h2[id],h3[id],h4[id],h5[id],h6[id]");
+     var sections = {};
+     var i = 0;
+     Array.prototype.forEach.call(section, function(e) {
+         sections[e.id] = e.offsetTop + 100;
+     });
+     console.log(stop);
+     window.onscroll = function(e) {
+         var scrollPosition = hasOffset ? window.pageYOffset : docBody.scrollTop;
+         
+         if (scrollPosition >= start && scrollPosition <= stop) {
+             // stick the div        
+             left.className = 'is-sticky';
+             left.style["top"] = '0px';
+         } else {
+             if (scrollPosition >= stop) {
+                console.log('stickypos: '+ scrollPosition);
+                left.className = 'is-sticky-end';
+                left.style["top"] = stop-scrollPosition+'px';
+             } else {
+                 (document.querySelector('#my_toc .active') != null) ? document.querySelector('#my_toc .active').setAttribute('class', ' '): null;
+                 // release the div
+                 left.className = '';
+             }
+         }
+
+
+
+         for (i in sections) {
+             if (sections[i] <= scrollPosition) {
+                 (document.querySelector('#my_toc .active') != null) ? document.querySelector('#my_toc .active').setAttribute('class', ' '): null;
+                 document.querySelector('#my_toc li a[href="#' + i + '"]').setAttribute('class', 'active');
+             }
+         }
+     };
+ })();
